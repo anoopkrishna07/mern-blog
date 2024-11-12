c423dc79d13501dfec1f96b3755385aa73be6ccb
anoopkrishna07 <harmonymusic2018@gmail.com>
2024-11-12 14:41:54 +0530

    show posts in dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { create, getPosts } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, create);
-router.get('/getposts', getPosts);
+router.get('/getposts', getposts);
 
 export default router;
